a965b4839ce53c7cd3a4d39c99d26c14a39c248a
Alex <asenges400@gmail.com>
2022-04-26 11:33:35 -0300

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-for char in range(1, 99 + 1):
-    if char == 99:
-        print(f"{char}")
-    else:
-        print(f"{char:02}", end=", ")
+for i in range(0, 9):
+    for j in range(0, 9 + 1):
+        if i >= j:
+            pass
+        elif i * 10 + j == 89:
+            print(f"{i}{j}")
+        else:
+            print(f"{i}{j}", end=", ")
+
+
