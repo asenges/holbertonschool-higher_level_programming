95390979cebd8a8c08cb5007da7f09bb06c89374
Alex <asenges400@gmail.com>
2022-04-26 07:26:31 -0300

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    power = 1
+    if b > 0 and a > 0:
+        for i in range (b):
+            power *= a
+        return power
+    elif b > 0 and a < 0:
+        for i in range (b):
+            power *= abs(a)
+        return -(power)
+    elif b < 0:
+        for i in range (abs(b)):
+            power *= a
+        return 1 / power    
+    else:
+        return 1
+
