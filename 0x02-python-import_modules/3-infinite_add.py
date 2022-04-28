e30067172ebdf2775737b66fb989a1f6a3e173c4
Alex <asenges400@gmail.com>
2022-04-28 09:56:17 -0300

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    sum = 0
+    if len(argv) == 1:
+        print("0")
+    if len(argv) > 1:
+        for i in range(1, len(argv)):
+            sum += int(argv[i], 10)
+        print("{:d}".format(sum))
