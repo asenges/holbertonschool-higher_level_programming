c9a0da4b246de96b8e49481b2d59da2b47ba1c93
Alex <asenges400@gmail.com>
2022-05-02 20:46:53 -0300

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,10 +3,7 @@
 def max_integer(my_list=[]):
 
     if my_list:
-        max_n = 0
-        for i in my_list:
-            if i > max_n:
-                max_n = i
-        return max_n
+        my_list.sort()
+        return my_list[-1]
     else:
         return None
