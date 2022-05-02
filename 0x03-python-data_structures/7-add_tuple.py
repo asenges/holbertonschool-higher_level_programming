456001a1212e1882016ae165889de415235da03f
Alex <asenges400@gmail.com>
2022-05-02 09:06:22 -0300

    0x03. Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a_v1, a_v2, b_v1, b_v2 = 0, 0, 0, 0
+
+    if len(tuple_a) == 0:
+        a_v1 = 0
+        a_v2 = 0
+    if len(tuple_a) == 1:
+        a_v1 = tuple_a[0]
+        a_v2 = 0
+    if len(tuple_a) > 1:
+        a_v1 = tuple_a[0]
+        a_v2 = tuple_a[1]
+    if len(tuple_b) == 0:
+        b_v1 = 0
+        b_v2 = 0
+    if len(tuple_b) == 1:
+        b_v1 = tuple_b[0]
+        b_v2 = 0
+    if len(tuple_b) > 1:
+        b_v1 = tuple_b[0]
+        b_v2 = tuple_b[1]
+
+    new_tuple = tuple()
+    new_tuple = (a_v1 + b_v1, a_v2 + b_v2)
+    
+    return new_tuple
