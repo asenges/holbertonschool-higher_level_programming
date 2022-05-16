84c2131fb980a7b2ab69145675bdbf76354f154c
Alex <asenges400@gmail.com>
2022-05-16 07:07:49 -0300

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    pitems = 0
+
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            pitems += 1
+        except IndexError:
+            break
+        except (ValueError, TypeError):
+            continue
+    print()
+    return pitems
