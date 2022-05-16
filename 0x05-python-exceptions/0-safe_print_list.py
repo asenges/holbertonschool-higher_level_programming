f1bb9e27968479e54ce789963ab07dc9d3439ad5
Alex <asenges400@gmail.com>
2022-05-16 13:32:38 -0300

    0x05-python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,9 @@
 
 def safe_print_list(my_list=[], x=0):
     pitems = 0
-
     for i in range(0, x):
         try:
-            print("{}".my_list[i], end='')
+            print("{d}".format(my_list[i]), end='')
             pitems += 1
         except IndexError:
             break
