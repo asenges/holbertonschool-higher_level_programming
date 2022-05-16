4ddd14ad381e9a67df1bb052c22d2536cff6d15b
Alex <asenges400@gmail.com>
2022-05-16 13:29:12 -0300

    0x05-python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@
 def safe_print_list(my_list=[], x=0):
     pitems = 0
 
-    for i in range (0, x):
+    for i in range(0, x):
         try:
             print("{d}".my_list[i], end='')
             pitems += 1
