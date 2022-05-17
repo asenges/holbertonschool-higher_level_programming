12a6acf8ac4ca6e455e2333a54989951a371c521
Alex <asenges400@gmail.com>
2022-05-17 08:22:46 -0300

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 2. Size validation """
+
+
+class Square:
+    """ class Square """
+    def __init__(self, size=0):
+        """ constructor method """
+        if int(size) < 0:
+            raise ValueError("size must be >= 0")
+        elif type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
