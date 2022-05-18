04e14c6f19bb993f5606284b6bfc9eae933f4943
Alex <asenges400@gmail.com>
2022-05-18 18:41:46 -0300

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -32,9 +32,9 @@ class Rectangle:
     def height(self, value):
         """ height setter """
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         elif not value >= 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
