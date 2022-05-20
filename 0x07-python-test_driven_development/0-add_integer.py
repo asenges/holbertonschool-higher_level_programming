b480047b87f8f02e9afb4809d5fce6ea57ff8eea
Alex <asenges400@gmail.com>
2022-05-20 06:44:14 -0300

    0x07. Python - Test-driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ This module returns the addition of two integers:
-@a and @b must be integers or floats, otherwise raise a TypeError 
+@a and @b must be integers or floats, otherwise raise a TypeError
 @a and @b must be first casted to integers if they are float
 Return: integer - the addition of a and b
 """
 
 
 def add_integer(a, b=98):
-    """ 
-    Return: additionof two integers.  
+    """
+    Return: additionof two integers.
     """
     if type(a) is float:
         a = int(a)
