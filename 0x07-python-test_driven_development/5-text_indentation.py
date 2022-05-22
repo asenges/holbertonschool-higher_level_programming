0be344fdcd37b73e41347539bb4c9e192993ecf8
Alex <asenges400@gmail.com>
2022-05-21 22:48:30 -0300

    0x07. Python - Test-driven development

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module prints a text with 2 new lines after ., ? and :
+@text must be a string, otherwise raise a TypeError
+There should be no space at the beginning or at the end of each printed line
+Return: None
+"""
+
+
+def text_indentation(text):
+    """
+    Return: None - prints a text
+    """
+    print_spacing = True
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    else:
+        for i in (text):
+            if i == '.' or i == '?' or i == ':':
+                print(i)
+                print()
+                print_spacing = False
+            else:
+                if print_spacing is False:
+                    print_spacing = True
+                    continue
+                else:
+                    print(i, end='')
