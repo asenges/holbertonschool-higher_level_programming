dbd44e93839436810eb4231b802ad86e20c73ce2
Alex <asenges400@gmail.com>
2022-05-30 11:22:58 -0300

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,13 @@ This module implements a class that inherits from list
 
 class MyList(list):
     """
-    This class inherits from list
+    This class inherits from class List
     """
-    def __init__(self):
-        pass
-
     def print_sorted(self):
+        """
+        This method prints a sorted list
+        """
+        for i in self:
+            if not isinstance(i, int):
+                raise TypeError("must be a list of integers")
         print(sorted(self))
