f7b88b17aa3cb6355b92b0f7f74944e22ea6b092
Alex <asenges400@gmail.com>
2022-05-31 10:37:54 -0300

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module adds all arguments to a Python list
+then save them to a file
+"""
+
+
+import json
+from sys import argv
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+try:
+    obj = load_from_json_file('add_item.json')
+    save_to_json_file(obj + argv[1:], 'add_item.json')
+except Exception as e:
+    save_to_json_file(argv[1:], 'add_item.json')
