943f7c76505f7d6236bf94a014c6b6d402d7670e
Alex <asenges400@gmail.com>
2022-06-04 17:57:41 -0300

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,16 @@ class Base:
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Saves to a file """
+        obj_list = []
+        name = str(cls.__name__) + ".json"
+        with open(name, 'w', encoding='utf8') as file:
+            if list_objs is None:
+                f.write(cls.to_json_string(obj_list))
+            else:
+                for obj in list_objs:
+                    obj_list.append(cls.to_dictionary(obj))
+                file.write(cls.to_json_string(obj_list))
