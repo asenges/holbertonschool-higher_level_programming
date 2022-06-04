b45e77b8b3a98e4e750c6f19909c1a6d21f9ad97
Alex <asenges400@gmail.com>
2022-06-04 17:10:58 -0300

    0x0C. Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module Square """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ class Square """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Object Builder """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ size getter """
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """ size setter """
+        self.width = size
+        self.height = size
+
+    def __str__(self):
+        """ str repr """
+        return ("[Square] ({}) {}/{} - {}"\
+        .format(self.id, self.x, self.y, super().width))
