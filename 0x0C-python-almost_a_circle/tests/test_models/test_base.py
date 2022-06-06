c29332d1912b863197a208b27b28ba949ac1d9d3
Alex <asenges400@gmail.com>
2022-06-06 17:18:25 -0300

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -117,6 +117,13 @@ class test_base(unittest.TestCase):
         base_new = Base.from_json_string("[]")
         self.assertEqual(base_new, [])
 
+    def test_documentation(self):
+        self.assertTrue(len(Base.__doc__) > 0)
+        self.assertTrue(len(Base.to_json_string.__doc__) > 0)
+        self.assertTrue(len(Base.save_to_file.__doc__) > 0)
+        self.assertTrue(len(Base.from_json_string.__doc__) > 0)
+        self.assertTrue(len(Base.create.__doc__) > 0)
+
 
 if __name__ == '__main__':
     unittest.main()
