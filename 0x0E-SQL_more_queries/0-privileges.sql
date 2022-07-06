00574c8b18d6b44c667a2fecb63b2f4afbf7e6b5
Alex <asenges400@gmail.com>
2022-07-06 07:47:52 -0300

    @asenges 2022-07-06 07:47:52

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- 0x0E. SQL - More queries
 -- Alex Senges
-SHOW GRANTS FOR 'user_0d_1i`@`localhost`, `user_0d_2`@`localhost`;
+SHOW GRANTS FOR `user_0d_1i`@`localhost`
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
 
