a9b044bb4fd6a6e4c072a7f5d58fe92332f43b9f
Alex <asenges400@gmail.com>
2022-07-06 19:42:34 -0300

    @asenges 2022-07-06 19:42:34

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,9 @@
 -- 0x0E. SQL - More queries
 -- Alex Senges
 SELECT 
-cities.id AS Id, 
-cities.name AS City, 
-states.name AS State
+cities.id, 
+cities.name, 
+states.name
 FROM cities 
 INNER JOIN states 
 ON cities.state_id = states.id;
