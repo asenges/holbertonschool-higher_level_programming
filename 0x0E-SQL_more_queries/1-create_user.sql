08264e0463b0726dd235a33b4e87d549fcb421cb
Alex <asenges400@gmail.com>
2022-07-06 07:54:04 -0300

    @asenges 2022-07-06 07:54:04

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- 0x0E. SQL - More queries
 -- Alex Senges
-CREATE User 
+CREATE USER 
 IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
 GRANT ALL PRIVILEGES ON * . * TO `user_0d_1`@`localhost`;
 FLUSH PRIVILEGES;
