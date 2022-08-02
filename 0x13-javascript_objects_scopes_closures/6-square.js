29ba99e98d2aa26b980fc1c4c1359c6abc70eb25
Alex <asenges400@gmail.com>
2022-08-02 20:26:08 -0300

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,21 @@
 #!/usr/bin/node
-const SquareBase = require('./5-square');
+const rectangle = require('./4-rectangle');
+module.exports = class Square extends rectangle {
 
-module.exports = class Square extends SquareBase {
-  charPrint (c) {
-    super.print(c);
+  constructor (size) {
+    super(size, size);
   }
-};
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        for (let j = 0; j < this.height; j++) {
+          process.stdout.write(c);
+        }
+        console.log('');
+      }
+    }
+  }
+}
