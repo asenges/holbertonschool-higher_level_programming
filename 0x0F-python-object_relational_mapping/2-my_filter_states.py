7fd6c0990ab08c35db743ae8759b5593d495e656
Alex <asenges400@gmail.com>
2022-08-07 19:13:57 -0300

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 """
-0. Get all states
+3. SQL Injection...
 """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     """
-    Main method
+    Main Method
     """
-    conn = MySQLdb.connect(host="localhost",
+    state = str(sys.argv[4])
+    conn = MySQLdb.connect(host='localhost',
                            port=3306,
                            user=sys.argv[1],
                            passwd=sys.argv[2],
                            db=sys.argv[3],
-                           charset="utf8")
+                           charset="utf8"
+                           )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states")
+    sql = """SELECT * FROM states WHERE name LIKE BINARY '{}'
+          ORDER BY id""".format(state)
+    cur.execute(sql)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
