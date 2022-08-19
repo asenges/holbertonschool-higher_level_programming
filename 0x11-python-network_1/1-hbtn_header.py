9f554811fd25cc28486edd8ead137e9d5d097d21
Alex <asenges400@gmail.com>
2022-08-19 19:31:44 -0300

    0x11. Python - Network#1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,5 +6,6 @@ from urllib import request
 from sys import argv 
 
 if __name__ == "__main__":
-    with request.urlopen(argv[1]) as res:
-        print(res.getheader("X-Request-Id"))
+    if argv[1] is not None:
+        with request.urlopen(argv[1]) as res:
+            print(res.getheader("X-Request-Id"))
