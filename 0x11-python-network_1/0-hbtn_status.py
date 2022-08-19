4dabd7d7f195f26ab69969b7ab194baad8de597b
Alex <asenges400@gmail.com>
2022-08-19 06:28:36 -0300

    0x11. Python - Network#1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+0. What's my status? #0
+"""
+import urllib.request as request
+
+
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
