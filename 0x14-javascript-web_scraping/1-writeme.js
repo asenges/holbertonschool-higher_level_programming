3355a9961a982458bd01cf94883d9147bd320437
Alex <asenges400@gmail.com>
2022-09-12 21:08:23 -0300

    0x14. JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// 1. Write me
+const fs = require('fs');
+
+const file = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(file, content, { flag: 'a+' }, err => {
+  if (err) {
+    console.error(err);
+  }
+});
